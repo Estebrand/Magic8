85e2b47c339212b2d9300ca88c36362d8492e45f
Esteban Urrea <estebrandg@gmail.com>
2017-11-16 13:55:09 -0500

    got the triangle in the right place

--- magic8.js
+++ magic8.js
@@ -30,3 +30,20 @@ $('submit').click(function()){
 			$('answer').html("no!");
 		};
 });
+
+
+// ANIMATIONS
+
+// $.fn.animateRotate = function(angle, duration, easing, complete) {
+//   var args = $.speed(duration, easing, complete);
+//   var step = args.step;
+//   return this.each(function(i, e) {
+//     args.complete = $.proxy(args.complete, e);
+//     args.step = function(now) {
+//       $.style(e, 'transform', 'rotate(' + now + 'deg)');
+//       if (step) return step.apply(e, arguments);
+//     };
+
+//     $({deg: 0}).animate({deg: angle}, args);
+//   });
+// };
